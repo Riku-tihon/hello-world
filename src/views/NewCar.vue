48eed711d55eb6e5dc4463a724b31307371207c1
Riku Tihon <tehno300817@gmail.com>
2021-04-25 11:18:39 +0300

    new final

--- src/views/NewCar.vue
+++ src/views/NewCar.vue
@@ -1,5 +1,5 @@
 <template><div class="newcar">
-  <input type="number" disabled v-model="car.id"><br>
+  <input type="number" disabled v-model="car.id" @change="noCar"><br>
   <input type="text" v-model="car.car_make" @change="noCar"><br>
   <input type="text" v-model="car.car_model" @change="noCar"><br>
   <input type="number" v-model="car.car_year" @change="noCar"><br>
@@ -21,16 +21,23 @@
         newCar()
         {
           this.$store.commit('newCar',{id:this.car.id,car_make:this.car.car_make,car_model:this.car.car_model,car_year:this.car_year,url:this.car.url,price:this.car.price});
-          this.car={};
-          this.state.commit('updateNoCar',{})
-          this.car.id= +this.$store.state.inventory[this.$store.state.inventory.length-1].id +1;
+          this.$store.commit('updateNoCar',{})
+          this.car.id=this.id;
+          this.$store.commit('updateNoCar',this.car)
+        },
+        noCar()
+        {
+          this.$store.commit('updateNoCar',this.car);
         }
       },
   mounted() {
-    this.car.id= +this.$store.state.inventory[this.$store.state.inventory.length-1].id +1;
+    this.car.id= this.id;
   },
-  noCar()
-{
-  this.$store.commit('updateNoCar',this.car);
-}
+  computed:
+      {
+        id()
+        {
+          return +this.$store.state.inventory[this.$store.state.inventory.length-1].id +1
+        }
+      }
 }</script>
