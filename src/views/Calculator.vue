0d0ab941fca57a387f4fb3021ac0b70829213fd7
Riku Tihon <tehno300817@gmail.com>
2021-05-01 09:08:35 +0300

    CALCULATOR with select without select

--- src/views/Calculator.vue
+++ src/views/Calculator.vue
@@ -1,31 +1,34 @@
-<template><div class="calculator">
+<template><div class="calculator" @click="changeScrollfromdown();changeScrolltodown()">
   <div>from</div>
-  <select v-model="froml">
-    <option>USD</option>
-    <option>CAD</option>
-    <option>CHF</option>
-    <option>EUR</option>
-    <option>GBP</option>
-  </select>
+  <div></div>
+  <span v-for="(item,index) in fromrates" :key="index"> <button @click="selectFrom(item)" :disabled="froml===item">{{item}}</button></span>
+  <button @click="changeScrollfrom($event);changeScrolltodown()" :disabled="!scrollfrom">V</button>
   <input type="number" v-model.lazy="fromNumber">
+  <div  class="scroll" :hidden="scrollfrom" @click="stp($event)">
+    <div><input v-model="fromExample" style="width: 90%"></div>
+    <div v-for="(value,name,index) in rates" :key="index"><button @click="selectFrom(name)" v-if="name.includes(fromExample.toUpperCase())" :disabled="froml===name" style="width: 100%">{{name}}</button></div></div>
   <div> to</div>
-  <select v-model="to">
-    <option>USD</option>
-    <option>CAD</option>
-    <option>CHF</option>
-    <option>EUR</option>
-    <option>GBP</option>
-  </select>
-  <input type="number" v-model.lazy="toNumber">
-</div></template>
+  <span v-for="(item,index) in torates" :key="index"> <button @click="selectTo(item)" :disabled="to===item">{{item}}</button></span>
+  <button @click="changeScrollto($event);changeScrollfromdown()" :disabled="!scrollto">V</button>
+  <input v-model="toNumber" disabled>
+  <div  class="scroll" :hidden="scrollto" @click="stp($event)">
+    <div><input v-model="toExample"></div>
+    <div v-for="(value,name,index) in rates" :key="index"><button @click="selectTo(name)" v-if="name.includes(toExample.toUpperCase())" :disabled="to===name" style="width: 100%">{{name}}</button></div></div>
+  <br>
+  <button @click="transfer">transfer</button>
+</div>
+</template>
 <script>
 import axios from "axios";
-//const apiKey='ffc83a532d7bc161548fe3379d4e7bc5'
 export default {
   name: 'Calculator',
   data()
   {
     return{
+      fromrates:['USD','EUR','MDL','RUB','RON','UAH'],
+      torates:['USD','EUR','MDL','RUB','RON','UAH'],
+      scrollfrom:true,
+      scrollto:true,
       fromNumber:0,
       toNumber:0,
       to:'USD',
@@ -33,52 +36,100 @@ export default {
       froml:'USD',
       toPath:1,
       fromPath:1,
+      rates:[],
+      fromExample:'',
+      toExample:'',
     }
   },
   watch:
       {
-        fromNumber (val)
-        {
-            axios.get(`http://api.exchangeratesapi.io/v1/latest?access_key=ffc83a532d7bc161548fe3379d4e7bc5`)
-                .then((response)=>{
-                  this.midle=val/(+response.data.rates[this.froml]);
-                  this.toNumber=this.midle*(+response.data.rates[this.to]);
-                })
-                .catch((error)=>{
-                  console.log(error)})
-        },
-        toNumber: function (val)
-        {
-          axios.get(`http://api.exchangeratesapi.io/v1/latest?access_key=ffc83a532d7bc161548fe3379d4e7bc5`)
-              .then((response)=>{
-                this.midle=val/(+response.data.rates[this.to]);
-                this.fromNumber=this.midle*(+response.data.rates[this.froml])
-              })
-              .catch((error)=>{
-                console.log(error)})
-        },
-        froml: function (val)
-        {
+      fromNumber:function (val)
+      {
+        axios.get(`http://api.exchangeratesapi.io/v1/latest?access_key=ffc83a532d7bc161548fe3379d4e7bc5`)
+            .then((response)=>{
+              this.midle=val/(+response.data.rates[this.froml]);
+              this.toNumber=this.midle*(+response.data.rates[this.to]);
+            })
+            .catch((error)=>{
+              console.log(error)})
+      }
+      },
+  computed:
+      {
 
-          axios.get(`http://api.exchangeratesapi.io/v1/latest?access_key=ffc83a532d7bc161548fe3379d4e7bc5`)
-              .then((response)=>{
-                this.midle=this.fromNumber/(+response.data.rates[val]);
-                this.toNumber=this.midle*(+response.data.rates[this.to]);
-              })
-              .catch((error)=>{
-                console.log(error)})
-        },
-        to:function (val)
+      },
+  methods:
+      {
+        transfer()
         {
-
+          if (this.to!==this.froml&&this.fromNumber!==0){
           axios.get(`http://api.exchangeratesapi.io/v1/latest?access_key=ffc83a532d7bc161548fe3379d4e7bc5`)
               .then((response)=>{
                 this.midle=this.fromNumber/(+response.data.rates[this.froml]);
-                this.toNumber=this.midle*(+response.data.rates[val]);
+                this.toNumber=this.midle*(+response.data.rates[this.to]);
               })
               .catch((error)=>{
-                console.log(error)})
+                console.log(error)})}
+          else
+          {
+            this.toNumber=this.fromNumber
+          }
+        },
+        changeScrollfrom(e)
+        {
+          e.stopPropagation()
+          this.scrollfrom=false;
+        },
+        changeScrollto(e)
+        {
+          e.stopPropagation()
+          this.scrollto=false;
+        },
+        changeScrollfromdown()
+        {
+          this.scrollfrom=true;
+          this.fromExample='';
+        },
+        changeScrolltodown()
+        {
+          this.scrollto=true;
+          this.toExample='';
+        },
+        stp(e)
+        {
+          e.stopPropagation()
+        },
+        selectFrom(name){
+          if (!this.fromrates.includes(name)){
+        this.fromrates.pop();
+        this.fromrates.push(name)}
+          this.froml=name;
+        },
+        selectTo(name)
+        {if (!this.torates.includes(name)){
+          this.torates.pop();
+          this.torates.push(name)}
+          this.to=name;
         }
-      }
+      },
+  mounted() {
+    axios.get(`http://api.exchangeratesapi.io/v1/latest?access_key=ffc83a532d7bc161548fe3379d4e7bc5`)
+        .then((response)=>{
+          this.rates=response.data.rates
+        })
+        .catch((error)=>{
+          console.log(error)})
+  }
 }
 </script>
+<style>
+.scroll
+{
+  position: relative;
+  left: 47.6%;
+  overflow-y: scroll;
+  height: 100px;
+  width: 100px;
+  text-align: center;
+}
+</style>
